7a89f8e0a2c784c963639e4e127c7ab2e678705a
Moses J <mosesjohansson@gmail.com>
2022-04-03 00:38:08 +0200

    init

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+const cors = require('cors')
+app.use(cors({origin: 'http://localhost:3000'}))
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    
+  console.log('a user connected');
+});
+
+server.listen(3330, () => {
+  console.log('listening on *:3330');
+});
