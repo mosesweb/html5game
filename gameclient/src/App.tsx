05e29705715cf2d45efdfb414329762b91580288
Moses J <mosesjohansson@gmail.com>
2022-04-03 18:51:51 +0200

    shoot red thing..

--- gameclient/src/App.tsx
+++ gameclient/src/App.tsx
@@ -2,24 +2,51 @@ import { useEffect, useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
 import { io } from "socket.io-client";
+import { fromEvent, map, Observable, switchMap, takeUntil } from 'rxjs';
 
 class MainGame {
-  gameArea: myGameArea
+  gameArea: GameArea
   myscore: GameComponent
-  myGamePiece: GameComponent
-  
-  myObstacles: any = [];
+  myGamePiece: GameCharacter
+
+  myObstacles: GameComponent[] = [];
+  keys: any = [];
+  click: boolean = false;
+  addedspecial: boolean = false;
 
   constructor() {
-    this.gameArea = new myGameArea();
+    this.gameArea = new GameArea();
     this.myscore = new GameComponent("30px", "Consolas", "black", 280, 40, "text", this.gameArea);
-    this.myGamePiece = new GameComponent(30, 30, "red", 10, 120, "", this.gameArea);
- }
+    this.myGamePiece = new GameCharacter(30, 30, "red", 10, 120, "", this.gameArea);
+    this.myGamePiece.gravity = 0;
+    console.log("main game")
+    this.gameArea.down$.subscribe((d: boolean[]) => {
+      this.keys = d;
+    });
+    this.gameArea.up$.subscribe(d => {
+      this.clearmove();
+    });
+    // mouse click
+    this.gameArea.clickup$.subscribe((d: boolean) => {
+      this.click = true;
+    });
+    this.gameArea.clickdown$.subscribe((d: boolean) => {
+      this.click = false;
+    });
 
+
+  }
+  clearmove() {
+    this.myGamePiece.speedX = 0;
+    this.myGamePiece.speedY = 0;
+  }
+
+  scoretext: string = "score ";
   updateGameArea = () => {
     var x, height, gap, minHeight, maxHeight, minGap, maxGap;
-    for (i = 0; i < this.myObstacles.length; i += 1) {
+    for (i = 0; i < this.myObstacles.filter(m => m.cancrash).length; i += 1) {
       if (this.myGamePiece.crashWith(this.myObstacles[i])) {
+        console.log("crash!")
         return;
       }
     }
@@ -30,20 +57,56 @@ class MainGame {
       minHeight = 20;
       maxHeight = 200;
       height = Math.floor(Math.random() * (maxHeight - minHeight + 1) + minHeight);
-      minGap = 50;
+      minGap = 100;
       maxGap = 200;
       gap = Math.floor(Math.random() * (maxGap - minGap + 1) + minGap);
       this.myObstacles.push(new GameComponent(10, height, "green", x, 0, "", this.gameArea));
       this.myObstacles.push(new GameComponent(10, x - height - gap, "green", x, height + gap, "", this.gameArea));
+      console.log("here..")
     }
     for (var i = 0; i < this.myObstacles.length; i += 1) {
-      this.myObstacles[i].x += -1;
+      if (this.myObstacles[i].cancrash)
+        this.myObstacles[i].x += -1; // move obstacle x <--
       this.myObstacles[i].update();
     }
-    this.myscore.text = "SCORE: " + this.gameArea.frameNo;
+    this.scoretext = "score (" + this.myObstacles.length + ") "
+
+    this.myscore.text = this.scoretext + this.gameArea.frameNo;
     this.myscore.update();
     this.myGamePiece.newPos();
     this.myGamePiece.update();
+
+    if (this.keys["KeyW"] || this.keys["KeyUp"]) {
+      this.myGamePiece.speedY = -1;
+    }
+    if (this.keys["KeyD"] || this.keys["KeyRight"]) {
+      this.myGamePiece.speedX = 1;
+    }
+    if (this.keys["KeyS"] || this.keys["KeyDown"]) {
+      this.myGamePiece.speedY = 1;
+    }
+    if (this.keys["KeyA"] || this.keys["KeyLeft"]) {
+      this.myGamePiece.speedX = -1;
+    }
+    if (this.keys["Space"]) {
+      {
+        //this.myGamePiece.speedX = 19;
+
+        console.log(this.myObstacles)
+        console.log("special!")
+        this.myscore.text = "SCORaaaEaa: " + this.gameArea.frameNo;
+        this.myscore.update();
+        this.myObstacles.push(new BallComponent(50, 50, "red", this.myGamePiece.x + 50, this.myGamePiece.y, "", this.gameArea));
+        console.log(this.myObstacles)
+        this.addedspecial = true;
+      }
+    }
+    // on click
+    if (this.click) {
+      if (!this.addedspecial) {
+
+      }
+    }
   }
 
   everyinterval = (n: any) => {
@@ -52,8 +115,7 @@ class MainGame {
   }
 
   start = () => {
-    this.gameArea.canvas.width = 480;
-    this.gameArea.canvas.height = 270;
+
     document.body.insertBefore(this.gameArea.canvas, document.body.childNodes[0]);
     this.gameArea.frameNo = 0;
     this.gameArea.interval = setInterval(this.updateGameArea, 20);
@@ -74,10 +136,11 @@ class GameComponent {
   ctx: any;
   color: any;
   text: string = "score"
+  cancrash: boolean = true;
 
-  gamearea: myGameArea
+  gamearea: GameArea
 
-  constructor(width: any, height: any, color: any, x: any, y: any, type: any, gamearea: myGameArea) {
+  constructor(width: any, height: any, color: any, x: any, y: any, type: any, gamearea: GameArea) {
     this.type = type;
     this.score = 0;
     this.width = width;
@@ -133,21 +196,76 @@ class GameComponent {
     return crash;
   }
 }
+class GameCharacter extends GameComponent {
+  accelerate = (n) => {
+    this.gravity = n;
+    return;
+  }
+}
 
-class myGameArea {
-  canvas: any
+class BallComponent extends GameComponent {
+  cancrash = false;
+  update = () => {
+    this.x += 1; // move obstacle x -->
+    this.ctx = this.gamearea.context;
+    if (this.type == "text") {
+      this.ctx.font = this.width + " " + this.height;
+      this.ctx.fillStyle = this.color;
+      this.ctx.fillText(this.text, this.x, this.y);
+    } else {
+      this.ctx.fillStyle = this.color;
+      this.ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+  }
+}
+
+class GameArea {
+  canvas: HTMLCanvasElement
   context: any
   frameNo: any
   interval: any
+  key: string = ""
+  keys: boolean[] = []
+  down$: Observable<any>
+  up$: Observable<boolean[]>
+
+  clickdown$: Observable<boolean>
+  clickup$: Observable<boolean>
+
   constructor() {
     this.canvas = document.createElement("canvas");
-    this.context = this.canvas.getContext("2d");
+    this.canvas.width = 1680;
+    this.canvas.height = 570; this.context = this.canvas.getContext("2d");
+
+    this.down$ = fromEvent(document, 'keydown', (e: any) => {
+      if (e.repeat) return this.keys;
+      this.keys[e.code] = true;
+      console.log("hey " + e.code)
+      console.log(this.keys)
+      return this.keys;
+    })
+    this.up$ = fromEvent(document, 'keyup', (e: any) => {
+      this.keys[e.code] = false;
+
+      return this.keys;
+    })
+    this.clickdown$ = fromEvent(document, 'mousedown', (e: any) => {
+      return true;
+    });
+    this.clickup$ = fromEvent(document, 'mouseup', (e: any) => {
+      return false;
+    });
   }
 
   clear = () => {
     this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
   }
-
+  controlDown = (event: KeyboardEvent) => {
+    this.key = event.code
+  };
+  controlUp = (event: KeyboardEvent) => {
+    this.key = ""
+  };
 
 }
 function App() {
@@ -157,46 +275,16 @@ function App() {
   const ioClient = io("http://localhost:3330", { transports: ['websocket'] });
   ioClient.emit('player move', { map: 4, coords: '0.0' });
   ioClient.on("seq-num", (msg: string) => console.info(msg));
-
   useEffect(() => {
-    var game: MainGame = new MainGame();
-
+    var game: MainGame;
+    game = new MainGame();
     game.start();
-  
-    const accelerate = (event, n) => {
-      game.myGamePiece.gravity = n;
-      return;
-    }
-  });
-
+  }, []);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <br />
-        <button >ACCELERATE</button>
-        <p>Use the ACCELERATE button to stay in the air</p>
-        <p>How long can you stay alive?</p>
+      <header >
         <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
           <a
             className="App-link"
             href="https://vitejs.dev/guide/features.html"
@@ -212,7 +300,4 @@ function App() {
 }
 
 export default App
-function componentDidMount() {
-  throw new Error('Function not implemented.');
-}
 
